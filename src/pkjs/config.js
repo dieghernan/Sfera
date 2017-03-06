7397fa16bcc168d493897dadbf29529ed573402d
dieghernan <diego.hernangomezherrero@gmail.com>
2017-03-06 12:13:22 +0100

    Clay page improved

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -30,7 +30,8 @@ module.exports = [
         "type": "color",
         "messageKey": "DotsColor",
         "defaultValue": "0x55AAFF",
-        "label": "Dots"
+        "label": "Dots",
+        "description": "Tip: Hide the dots by setting the same color than Background"
       }      
     ]
   },
@@ -58,25 +59,32 @@ module.exports = [
         "type": "toggle",
         "messageKey": "WeatherUnit",
         "label": "Temperature in Fahrenheit",
-        "defaultValue": false      
+        "defaultValue": false,
+        "description": "Applicable if Weather toggle is active"
       },
       {
         "type": "toggle",
         "messageKey": "DisplayLoc",
         "label": "Location",
-        "defaultValue": false      
+        "defaultValue": false,
+        "description": "When available, neighbourhood is displayed instead of city"
       },
     ]
  },
  //Yahoo disclaimer
   {
       "type": "heading",
-      "defaultValue": "Weather info provided by <a href=https://www.yahoo.com/?ilc=401>Yahoo.com </a>",
+      "defaultValue": "Weather and Location info provided by <a href=https://www.yahoo.com/?ilc=401>Yahoo.com </a>",
       "size":6
   },
       
  {
     "type": "submit",
     "defaultValue":"Ok"
-  }
+  },
+  {
+      "type": "heading",
+      "defaultValue": "Made with ♥ in Madrid, Spain",
+      "size":6
+  }    
 ];
